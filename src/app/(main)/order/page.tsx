661e2a8ee0ec5ccd6e209f3a61e30c15271e9178
Petr Kabourek <242739@muni.cz>
2024-12-13 23:59:51 +0100

    add card to create order form

--- src/app/(main)/order/page.tsx
+++ src/app/(main)/order/page.tsx
@@ -1,14 +1,18 @@
 import CreateOrderForm from '@/components/form/orders/create-order-form';
+import { Card } from '@/components/ui/card';
 import { getAvailableCommodities } from '@/server-actions/commodities';
 
 const OrderPage = async () => {
 	const commodities = await getAvailableCommodities();
 	return (
 		<div className="p-4">
-			<h2 className="mb-4 text-4xl">Create new order</h2>
-			<CreateOrderForm
-				commodities={commodities.map(commodity => commodity.name)}
-			/>
+			<Card>
+				<h2 className="mb-4 text-4xl">Create new order</h2>
+
+				<CreateOrderForm
+					commodities={commodities.map(commodity => commodity.name)}
+				/>
+			</Card>
 		</div>
 	);
 };
