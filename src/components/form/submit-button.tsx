9a5b6e6f940ca2880438efb6efe2ec4ccf0601fb
silox <edruisl99@gmail.com>
2024-12-13 22:51:22 +0100

    Add better logo

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -12,6 +12,7 @@ export const SubmitButton = ({
 	children
 }: PropsWithChildren<SubmitButtonProps>) => (
 	<Button
+		colorType="secondary"
 		className="flex w-full justify-center"
 		type="submit"
 		disabled={isLoading}
