a9c7524c2584f48325b09dd479a7f1a3b0956183
silox <edruisl99@gmail.com>
2024-12-14 22:19:13 +0100

    Add mobile navbar

--- src/components/navigation/user-navigation.tsx
+++ src/components/navigation/user-navigation.tsx
@@ -4,14 +4,12 @@ import { signOut } from 'next-auth/react';
 import { Button } from '../ui/button';
 
 const UserNavigation = () => (
-	<div className="hidden self-center bg-secondary-dark md:block">
-		<Button
-			className="rounded-none bg-secondary-dark p-5 text-gray-200 hover:bg-inherit hover:text-white active:scale-100"
-			onClick={() => signOut()}
-		>
-			Log out
-		</Button>
-	</div>
+	<Button
+		className="rounded-lg bg-secondary-dark px-6 py-2 text-gray-200 hover:bg-secondary-dark hover:text-white active:scale-100 md:rounded-none md:p-5"
+		onClick={() => signOut()}
+	>
+		Log out
+	</Button>
 );
 
 export default UserNavigation;
