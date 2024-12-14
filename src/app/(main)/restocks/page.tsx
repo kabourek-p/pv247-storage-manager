8a46c109b1d5a0ee2b595377897b32dd0fc36316
Karolina Krommelova <514365@mail.muni.cz>
2024-12-14 18:58:14 +0100

    removed redirect in row

--- src/app/(main)/restocks/page.tsx
+++ src/app/(main)/restocks/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { redirect } from 'next/navigation';
 
 import AddRestockButton from '@/components/restocks/add-restock-button';
 import { getRestockRows } from '@/server-actions/restocks';
@@ -10,21 +9,11 @@ import { restockColumns } from '@/components/restocks/restock-table-column';
 const Restocks = async () => {
 	const data = await getRestockRows();
 
-	const handleRowClick = async (id: string) => {
-		'use server';
-		redirect(`/order/${id}`);
-	};
-
 	return (
 		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
 			<Card>
 				<AddRestockButton />
-				<DataTable
-					data={data}
-					columns={restockColumns}
-					rowClickHandler={handleRowClick}
-					filter="commodity"
-				/>
+				<DataTable data={data} columns={restockColumns} filter="commodity" />
 			</Card>
 		</div>
 	);
