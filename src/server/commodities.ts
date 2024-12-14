e870d098d76efddce3d93cad0b878d2bb7d50bf4
silox <edruisl99@gmail.com>
2024-12-14 22:58:23 +0100

    Remove random user / commodity generation

--- src/server/commodities.ts
+++ src/server/commodities.ts
@@ -1,19 +1,10 @@
 'use server';
 
-import generateRandomString from '@/server/utils';
 import { type CommodityFormSchema } from '@/components/form/commodities/commodity-form';
 
 import prisma from '../lib/prisma';
 
 export const getCommodities = async () => prisma.commodity.findMany();
-
-export const createRandomCommodity = async () =>
-	prisma.commodity.create({
-		data: {
-			name: generateRandomString(10),
-			unit: 'KG'
-		}
-	});
 
 export const createCommodity = async (commodity: CommodityFormSchema) =>
 	prisma.commodity.create({
