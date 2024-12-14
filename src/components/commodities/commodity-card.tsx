2ae076ba70af3239331b294bc5f2a71f60024794
Karolina Krommelova <514365@mail.muni.cz>
2024-12-14 09:20:33 +0100

    smaller cards

--- src/components/commodities/commodity-card.tsx
+++ src/components/commodities/commodity-card.tsx
@@ -21,19 +21,19 @@ const CommodityCard: React.FC<CommodityCardProps> = ({
 		unitPrice instanceof Decimal ? unitPrice.toString() : unitPrice;
 
 	return (
-		<div className="flex h-80 w-72 flex-col items-center justify-between rounded-lg border border-black bg-[#EFD5BE] p-6 text-black shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-2xl">
-			<div className="self-start text-xl font-semibold text-gray-800">
+		<div className="flex h-64 w-56 flex-col items-center justify-between rounded-lg border border-black bg-secondary-light p-4 text-black shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-2xl">
+			<div className="self-start text-lg font-semibold text-gray-800">
 				{name}
 			</div>
 
 			<div className="flex w-full flex-grow items-center justify-center">
-				<div className="text-5xl font-extrabold text-gray-900">
+				<div className="text-4xl font-extrabold text-gray-900">
 					{formattedQuantity}
 				</div>
-				<div className="ml-2 text-2xl font-medium text-gray-700">{unit}</div>
+				<div className="ml-2 text-xl font-medium text-gray-700">{unit}S</div>
 			</div>
 
-			<div className="mt-4 self-start text-2xl font-normal text-gray-700">
+			<div className="mt-3 self-start text-xl font-normal text-gray-700">
 				{formattedUnitPrice} CZK
 			</div>
 		</div>
