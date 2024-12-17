0c5a74bc419966dfe7f0420fe7fecb7d04d0611c
Petr Kabourek <242739@muni.cz>
2024-12-18 00:48:23 +0100

    add other back arrow buttons

--- src/app/(main)/order/[id]/edit/page.tsx
+++ src/app/(main)/order/[id]/edit/page.tsx
@@ -7,6 +7,8 @@ import EditOrderForm from '@/components/form/orders/edit-order-form';
 import { getOrderData } from '@/server-actions/orders';
 import { Card } from '@/components/card';
 import authUser from '@/lib/auth';
+import {BackArrowButton} from "@/components/ui/back-arrow-button";
+import React from "react";
 
 const OrderEditPage = async ({
 	params
@@ -31,6 +33,9 @@ const OrderEditPage = async ({
 	return (
 		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
 			<Card>
+				<div className="ml-4 sm:ml-0">
+					<BackArrowButton/>
+				</div>
 				<h2 className="mb-8 pl-4 pt-4 text-4xl">Edit order</h2>
 				<EditOrderForm
 					defaultValues={orderData}
--- src/app/(main)/order/[id]/page.tsx
+++ src/app/(main)/order/[id]/page.tsx
@@ -8,6 +8,7 @@ import EditOrderButton from '@/components/orders/edit-order-button';
 import LockOrderButton from '@/components/orders/lock-order-button';
 import { Card } from '@/components/card';
 import authUser from '@/lib/auth';
+import {BackArrowButton} from "@/components/ui/back-arrow-button";
 
 const Page = async ({ params }: { params: Promise<{ id: number }> }) => {
 	const user = await authUser();
@@ -26,14 +27,17 @@ const Page = async ({ params }: { params: Promise<{ id: number }> }) => {
 	return (
 		<div className="flex min-h-screen grid-rows-[20px_1fr_20px] flex-col gap-16 bg-white p-8 pt-20 text-center font-[family-name:var(--font-geist-sans)] sm:bg-gray-100 sm:p-20 sm:text-left">
 			<Card className="pb-10">
+				<div className="ml-4 sm:ml-0">
+					<BackArrowButton/>
+				</div>
 				<div>
 					<h2 className="py-4 text-4xl sm:p-4">
 						Details of order: {order?.note}
 					</h2>
 				</div>
 				<div className="flex space-x-3 p-4 pt-0">
-					<EditOrderButton order={order} />
-					<LockOrderButton order={order} />
+					<EditOrderButton order={order}/>
+					<LockOrderButton order={order}/>
 				</div>
 				{order?.invoice && (
 					<span className="text-gray-700">
